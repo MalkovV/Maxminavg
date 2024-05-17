ca981723e291e4f0ee06c29c0da366d7011aca5e
Vyacheslav <malkoff.va@gmail.com>
2024-05-17 10:26:34 +0500

    +bubble sort

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        int n = 20;
+        int n = 10;
         double[] digits = new double[n];
         System.out.printf("Задан массив из %d элементов:\n", n);
         for (int i = 0; i < digits.length; i++) {
             digits[i] = Math.random() * 100;
-            System.out.printf("%.0f ", digits[i]);
+            System.out.printf("%.2f ", digits[i]);
         }
+        System.out.println();
 
         double max = digits[0];
         double min = digits[0];
@@ -20,8 +21,22 @@ public class Main {
             avg += digits[i] / digits.length;
         }
 
-        System.out.printf("\nМаксимальное значение элемента массива: %.0f", max);
-        System.out.printf("\nМинимальное значение элемента массива: %.0f", min);
-        System.out.printf("\nСреднее значение всех элементов массива: %.2f", avg);
+        System.out.printf("\nМаксимальное значение элемента массива: %.2f", max);
+        System.out.printf("\nМинимальное значение элемента массива: %.2f", min);
+        System.out.printf("\nСреднее значение всех элементов массива: %.2f \n\n", avg);
+
+        for (int i = 0; i < digits.length; i++) {
+            for (int j = 0; j < digits.length - i - 1; j++) {
+                if (digits[j] > digits[j + 1]) {
+                    double temp = digits[j];
+                    digits[j] = digits[j + 1];
+                    digits[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < digits.length; i++) {
+            System.out.printf("Элемент массива с индексом %d:  %.2f \n",i, digits[i]);
+        }
     }
 }
